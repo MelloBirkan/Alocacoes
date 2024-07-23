fc51aa3dabba198e4a3ba7590b758dc0c8e4209c
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-07-23 20:14:33 -0300

    Initial Commit

---
+++ Alocacoes/AlocacoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlocacoesApp.swift
+//  Alocacoes
+//
+//  Created by Marcello Gonzatto Birkan on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AlocacoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
