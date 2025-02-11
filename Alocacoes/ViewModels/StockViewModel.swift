ade9e2753a37cc657ae6ae1d1981db019e2d8379
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-02-11 00:40:37 -0300

    Feat: Add StocksViewModel and AddStockView for stock management

---
+++ Alocacoes/ViewModels/StockViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StockViewModel.swift
+//  Alocacoes
+//
+//  Created by Marcello Gonzatto Birkan on 11/02/25.
+//
+
+import Foundation
---
+++ Alocacoes/Views/Stocks/AddStockView.swift
@@ -0,0 +1,18 @@
+//
+//  AddStockView.swift
+//  Alocacoes
+//
+//  Created by Marcello Gonzatto Birkan on 11/02/25.
+//
+
+import SwiftUI
+
+struct AddStockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddStockView()
+}
